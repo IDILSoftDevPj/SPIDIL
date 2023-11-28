5729076de3564e60dddf51b80979148213437a38
Quaglia Victor <150698148+Vetti-Vetto@users.noreply.github.com>
2023-11-28 11:09:59 +0100

    Added a code to set a working directory to find the txt file

--- pygen/words/words_in_proteome.py
+++ pygen/words/words_in_proteome.py
@@ -2,6 +2,22 @@
 
 import os
 
+def find_file(directory, file_name):
+    """
+    Find a file in the specified directory.
+
+    Args:
+    - directory (str): The directory to search in.
+    - file_name (str): The name of the file to find.
+
+    Returns:
+    - str: The full path to the found file, or None if not found.
+    """
+    for root, dirs, files in os.walk(directory):
+        if file_name in files:
+            return os.path.join(root, file_name)
+    return None
+
 def read_words(file_path):
     """
     Read words from a file, convert them to uppercase, and filter words
@@ -25,18 +41,26 @@ def read_words(file_path):
         return []
 
 if __name__ == "__main__":
-    # Specify the folder where the file is located
-    folder_path = "C:/Users/Skydr/OneDrive/Documents/M1 IDIL ECO EVO/software development/project files txt/"
+    # Specify the directory to search for files
+    search_directory = "C:/Users/Skydr/OneDrive/Documents/M1 IDIL ECO EVO/software development/project files txt/"
 
-    # Construct the full file path
-    file_name = "english-common-words.txt"
-    file_path = os.path.join(folder_path, file_name)
-
-    result = read_words(file_path)
-
-    if result:
-        print("Words in uppercase with 3 or more characters:")
-        print(result)
-        print(f"Number of selected words: {len(result)}")
+    # Find the words file
+    words_file_path = find_file(search_directory, "english-common-words.txt")
+    if words_file_path is None:
+        print("Error: 'english-common-words.txt' not found.")
     else:
-        print("No words selected.")
+        print(f"Found 'english-common-words.txt' at: {words_file_path}")
+
+    # Continue with the rest of your script using the found file paths
+    # ...
+
+    if words_file_path :
+        # Read words
+        words_result = read_words(words_file_path)
+
+        if words_result:
+            print("\nWords in uppercase with 3 or more characters:")
+            print(words_result)
+            print(f"Number of selected words: {len(words_result)}")
+        else:
+            print("No words selected.")
