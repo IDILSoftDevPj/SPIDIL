af175575d604a04d5238cafceb2465065434f810
Mountassira MATIN <mountassira.matin@etu.umontpellier.fr>
2023-12-10 11:22:03 +0100

    added the extract_organism function onto the genbank2fasta py file

--- pygen/convert/genbank2fasta.py
+++ pygen/convert/genbank2fasta.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def read_file(file_path):
     try:
@@ -10,6 +11,14 @@ def read_file(file_path):
         print(f"Error: File '{file_path}' not found.")
         return []
 
+def extract_organism(file_content):
+    organism_pattern = re.compile(r'^\s*ORGANISM\s*(.*?)\s*$', re.MULTILINE)
+    match = organism_pattern.search("".join(file_content))
+    if match:
+        return match.group(1)
+    else:
+        return "Organism name not found."
+
 # Define the file path
 directory = "C:/Users/India ELLIOTT/Documents/SPIDIL"
 file_name = "NC_001133.gbk"
@@ -17,6 +26,9 @@ file_path = os.path.join(directory, file_name)
 
 # Call the read_file() function
 file_content = read_file(file_path)
+
+# Call the extract_organism() function
+organism_name = extract_organism(file_content)
 
 # You can now use the 'file_content' list for further processing
 # For example, printing the first 10 lines:
@@ -29,3 +41,6 @@ file_content = read_file(file_path)
 print("All the lines")
 for line in file_content:
     print(line.strip())
+
+# Print the organism name
+print(f"Organism name: {organism_name}")
