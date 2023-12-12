09676345d0f13c466a8f2227cd0d0e6ccb8ffa08
india <india.elliott@etu.umontpellier.fr>
2023-12-12 11:07:08 +0100

    ...

--- pygen/convert/genbank2fasta.py
+++ pygen/convert/genbank2fasta.py
@@ -19,32 +19,8 @@ def extract_organism(file_content):
     else:
         return "Organism name not found."
 
-def find_genes(file_content):
-    genes = []
-
-    for line in file_content:
-        if line.startswith('     gene            '):  # Check if the line indicates a gene
-            is_antisense = 'complement' in line  # Check if it's an antisense gene
-            line = line.replace('     gene            ', '')  # Remove unnecessary prefix
-            line = line.replace('<', '').replace('>', '')  # Remove < and > symbols
-
-            # Extract the start and end positions
-            positions = [int(pos.strip()) for pos in line.replace('complement(', '').replace(')', '').split('..')]
-
-            if len(positions) == 1:
-                start = end = positions[0]
-            else:
-                start, end = positions
-
-            if is_antisense:
-                genes.append([start, end, 'antisense'])
-            else:
-                genes.append([start, end, 'sense'])
-
-    return genes
-
 # Define the file path
-directory = "C:/Users/teren/Desktop/softdev_project"
+directory = "C:/Users/India ELLIOTT/Documents/SPIDIL"
 file_name = "NC_001133.gbk"
 file_path = os.path.join(directory, file_name)
 
@@ -53,9 +29,6 @@ file_content = read_file(file_path)
 
 # Call the extract_organism() function
 organism_name = extract_organism(file_content)
-
-# Call the find_genes() function
-genes = find_genes(file_content)
 
 # You can now use the 'file_content' list for further processing
 # For example, printing the first 10 lines:
@@ -71,11 +44,6 @@ for line in file_content:
 
 # Print the organism name
 print(f"Organism name: {organism_name}")
-
-# Print information about each gene
-print("Information about each gene:")
-for gene in genes:
-    print(f"Start: {gene[0]}, End: {gene[1]}, Type: {gene[2]}")
 
 # Reverse complementory sequence
 def construct_comp_inverse(dna_sequence):
@@ -111,22 +79,3 @@ print("Result 2:", result2)
 print("\nOriginal Sequence 3:", sequence3)
 print("Result 3:", result3)
 
-#writing a fasta file
-
-def write_fasta(file_name, comment, sequence):
-    # Open the file in write mode
-    with open(file_name, 'w') as fasta_file:
-        # Write the comment line
-        fasta_file.write(f'>{comment}\n')
-
-        # Write the sequence in lines of 80 characters
-        for i in range(0, len(sequence), 80):
-            line = sequence[i:i+80]
-            fasta_file.write(f'{line}\n')
-
-# Test the function
-file_name = 'test.fasta'
-comment = 'my comment'
-sequence = 'atcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcg'
-
-write_fasta(file_name, comment, sequence)
